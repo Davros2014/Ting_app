229274d825f703ec9526ef2935f256c4cc079e13
David Parsons <info@dpdesigns.eu>
2021-03-27 22:43:50 +0100

    updated npm scripts

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
+
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -8,7 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static("./public" || "client/build"));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //routes
 app.use("/dashboard", require("./routes/dashboard"));
