3523afd1b9da4410097e0b16ba3b16fd0d863f56
David Parsons <info@dpdesigns.eu>
2021-03-27 15:26:07 +0100

    moved server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(express.static("./public"));
 
 //routes
-app.use("/dashboard", require("./routes/dashboard"));
-app.use("/authentication", require("./routes/jwtAuth"));
+app.use("/dashboard", require("./server/routes/dashboard"));
+app.use("/authentication", require("./server/routes/jwtAuth"));
 
 app.listen(port, () => {
     console.log(`Server is starting on port ${port}`);
