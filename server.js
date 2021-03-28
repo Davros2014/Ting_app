9805bfc60a3e6c4d405f71b8507e08fb05e9e3a6
David Parsons <info@dpdesigns.eu>
2021-03-28 09:37:03 +0200

    resolving routing issues

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -10,11 +9,14 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+// app.use(express.static("./public"));
+
 app.use(express.static(path.join(__dirname, "client/build")));
 
 //routes
-app.use("api/dashboard", require("./routes/dashboard"));
-app.use("api/authentication", require("./routes/jwtAuth"));
+app.use("/api/dashboard", require("./routes/dashboard"));
+app.use("/api/authentication", require("./routes/jwtAuth"));
 
 app.listen(port, () => {
     console.log(`Server is starting on port ${port}`);
